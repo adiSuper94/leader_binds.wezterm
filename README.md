b7a0eb48e0f646f4f028b775e0b54d6ffb755ad3
Aditya Subramanian <aditarun2008@gmail.com>
2026-04-19 12:27:49 -0400

    more opts

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-# Wezterm Leader bindings
+# Wezterm Leader Bindings
 
-Applies default bindings for wezterm leader key
+Configure leader key bindings that align with Wezterm's default keybindings.
+
+## Default Bindings
+
+| Keys | Action |
+|------|--------|
+| `LEADER + <Arrow>` | Navigate panes |
+| `LEADER \| SHIFT + "`|  Split vertically |
+| `LEADER \| SHIFT + %`|  Split horizontally |
+
+## Configuration
+
+```lua
+local wezterm = require("wezterm")
+local leader_binds = require("https://gitlab.com/adiSuper94/leader_binds.wezterm")
+
+local config = wezterm.config_builder()
+leader_binds.apply_to_config(config, {}) -- You can omit the empty table if you don't want to override any default bindings
+return config
+```
+
+## Default Options
+
+```lua
+{
+  navigation = {
+    vim = false,           -- Enable vim-style `h/j/k/l` pane navigation
+  },
+  split = {
+    vim = false -- Enable vim-style splits with `v` and `s`
+  },
+  resize = {
+    vim = false,    -- Enable vim-style resize with `H/J/K/L`
+    pane_resize = 5 -- Number of cells to resize
+  },
+}
+```
+
+
+
--- plugin/init.lua
+++ plugin/init.lua
@@ -3,41 +3,92 @@ local M = {}
 local wezterm = require("wezterm")
 local act = wezterm.action
 
----@param config unknown
----@param opts { vim_binds: boolean?, pane_navigation: boolean? }
-function M.apply_to_config(config, opts)
-  local keys = {
-    -- Navigating Panes
-    { key = "LeftArrow",  mods = "LEADER",         action = act.ActivatePaneDirection("Left") },
-    { key = "RightArrow", mods = "LEADER",         action = act.ActivatePaneDirection("Down") },
-    { key = "UpArrow",    mods = "LEADER",         action = act.ActivatePaneDirection("Up") },
-    { key = "DownArrow",  mods = "LEADER",         action = act.ActivatePaneDirection("Right") },
+---@alias navivagtion_opts { vim: boolean }
+---@alias split_opts { vim: boolean? }
+---@alias resize_opts { vim: boolean?, pane_resize: number? }
+---@alias Opts { split: split_opts?, navigation: navivagtion_opts?, resize: resize_opts? }
 
+---@type Opts
+local default_opts = {
+  split = { vim = false },
+  navigation = { vim = false },
+  resize = { vim = false, pane_resize = 5 },
+}
+local function merge(mod, base)
+  for k, v in pairs(base) do
+    if type(v) == "table" and type(mod[k]) == "table" then
+      merge(mod[k], v)
+    elseif mod[k] == nil then
+      mod[k] = v
+    end
+  end
+end
+
+---@param config unknown
+---@param opts Opts
+function M.apply_to_config(config, opts)
+  merge(opts or {}, default_opts)
+  local resize = opts.resize.pane_resize
+  local keys = {
+    -- SHIFT | ALT | CTRL
     -- Splitting Panes
     { key = "%",          mods = "LEADER | SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
     { key = "\"",         mods = "LEADER | SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
+    -- Resizing Panes
+    { key = "LeftArrow",  mods = "LEADER | SHIFT", action = act.AdjustPaneSize({ "Left", resize }) },
+    { key = "DownArrow",  mods = "LEADER | SHIFT", action = act.AdjustPaneSize({ "Down", resize }) },
+    { key = "UpArrow",    mods = "LEADER | SHIFT", action = act.AdjustPaneSize({ "Up", resize }) },
+    { key = "RightArrow", mods = "LEADER | SHIFT", action = act.AdjustPaneSize({ "Right", resize }) },
+
+
+    -- SHIFT | CTRL
+    -- Navigating Panes
+    { key = "LeftArrow",  mods = "LEADER",         action = act.ActivatePaneDirection("Left") },
+    { key = "DownArrow",  mods = "LEADER",         action = act.ActivatePaneDirection("Down") },
+    { key = "UpArrow",    mods = "LEADER",         action = act.ActivatePaneDirection("Up") },
+    { key = "RightArrow", mods = "LEADER",         action = act.ActivatePaneDirection("Right") },
+
+    -- Navigating Tabs
+    { key = "Tab",        mods = "LEADER",         action = act.ActivateTabRelative(1) },
+    { key = "Tab",        mods = "LEADER | SHIFT", action = act.ActivateTabRelative(-1) },
+    { key = "9",          mods = "LEADER",         action = act.ActivateTab(-1) },
+    { key = "{",          mods = "LEADER",         action = act.ActivateTabRelative(-1) },
+    { key = "}",          mods = "LEADER",         action = act.ActivateTabRelative(1) },
+
+    { key = "r",          mods = "LEADER",         action = act.ReloadConfiguration },
+    { key = "t",          mods = "LEADER",         action = act.SpawnTab("CurrentPaneDomain") },
+    { key = "w",          mods = "LEADER",         action = act.CloseCurrentPane({ confirm = true }) },
+    { key = "z",          mods = "LEADER",         action = act.TogglePaneZoomState },
+
   }
-  if opts and opts.vim_binds then
+
+  for i = 1, 8 do
+    table.insert(keys, {
+      key = tostring(i),
+      mods = "LEADER",
+      action = act.ActivateTab(i - 1),
+    })
+  end
+  if opts.navigation.vim then
     table.insert(keys, { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") })
     table.insert(keys, { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") })
     table.insert(keys, { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") })
     table.insert(keys, { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") })
   end
 
-  if opts and opts.pane_navigation then
-    for i = 1, 9 do
-      table.insert(keys, {
-        key = tostring(i),
-        mods = "LEADER",
-        action = act.ActivateTab(i - 1),
-      })
-      table.insert(keys, {
-        key = tostring(0),
-        mods = "LEADER",
-        action = act.ActivateTab(9),
-      })
-    end
+  if opts.split.vim then
+    table.insert(keys,
+      { key = "v", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) })
+    table.insert(keys,
+      { key = "s", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) })
+  end
+
+  if opts.resize.vim then
+    table.insert(keys, { key = "h", mods = "LEADER | SHIFT", action = act.AdjustPaneSize({ "Left", resize }) })
+    table.insert(keys, { key = "j", mods = "LEADER | SHIFT", action = act.AdjustPaneSize({ "Down", resize }) })
+    table.insert(keys, { key = "k", mods = "LEADER | SHIFT", action = act.AdjustPaneSize({ "Up", resize }) })
+    table.insert(keys, { key = "l", mods = "LEADER | SHIFT", action = act.AdjustPaneSize({ "Right", resize }) })
   end
 
   if not config.keys then
@@ -45,10 +96,6 @@ function M.apply_to_config(config, opts)
   end
   for _, key in ipairs(keys) do
     table.insert(config.keys, key)
-  end
-
-  if not config.key_tables then
-    config.key_tables = {}
   end
 end
 
